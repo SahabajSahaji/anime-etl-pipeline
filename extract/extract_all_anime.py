0b0ad0150095a140069079f686020a9c9225fa53
SahabajSahaji <sahabajsahaji@gmail.com>
2026-05-18 04:23:44 +0530

    Added pages from 800 to 999

--- extract/extract_all_anime.py
+++ extract/extract_all_anime.py
@@ -8,10 +8,10 @@ def extract_all_anime():
     #create folder if not exists
     os.makedirs("data/raw",exist_ok=True)
 
-    page=600
+    page=800
     has_next_page=True
 
-    while has_next_page and page <800:
+    while has_next_page and page <1000:
         try:
 
             url=f"https://api.jikan.moe/v4/anime?page={page}"
