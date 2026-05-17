2acf4261683b4954630340b45bd673bed2b0b9c2
SahabajSahaji <sahabajsahaji@gmail.com>
2026-05-18 03:39:39 +0530

    Added from page 200 to 399

--- extract/extract_all_anime.py
+++ extract/extract_all_anime.py
@@ -8,10 +8,10 @@ def extract_all_anime():
     #create folder if not exists
     os.makedirs("data/raw",exist_ok=True)
 
-    page=50
+    page=200
     has_next_page=True
 
-    while has_next_page and page <200:
+    while has_next_page and page <400:
         try:
 
             url=f"https://api.jikan.moe/v4/anime?page={page}"
