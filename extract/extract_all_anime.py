83eea90016ebcd58bdee159cf00bee231e6fe150
SahabajSahaji <sahabajsahaji@gmail.com>
2026-05-18 03:23:33 +0530

    Added json file from page 1 to page 199

---
+++ extract/extract_all_anime.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import time
+import os
+
+
+def extract_all_anime():
+    #create folder if not exists
+    os.makedirs("data/raw",exist_ok=True)
+
+    page=50
+    has_next_page=True
+
+    while has_next_page and page <200:
+        try:
+
+            url=f"https://api.jikan.moe/v4/anime?page={page}"
+
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+
+            #save raw json page 
+            file_path=f"data/raw/anime_page_{page}.json"
+
+            with open(file_path,"w",encoding="utf-8") as file:
+                json.dump(data,file,indent=4)
+
+            print(f"Saved page {page}")
+
+            #check next page
+            has_next_page=data["pagination"]["has_next_page"]
+
+            page+=1
+
+            #avoid rate limit
+            time.sleep(2)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error on page {page}:{e}")
+            
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from extract.extract_api import extract_anime
+from extract.extract_all_anime import extract_all_anime
 
 
 def run_pipeline():
-    df=extract_anime()
-    print(df.head())
+    # df=extract_anime()
+    # print(df.head())
 
-    df.to_csv("data/raw/anime.csv",index=False)
+    # df.to_csv("data/raw/anime.csv",index=False)
+    extract_all_anime()
+
+
 
 
 if __name__ =="__main__":
